3c79f46a8c8ebaa1e159f22582e1d482bcd59d1c
MrKekovich <119677210+MrKekovich@users.noreply.github.com>
2023-04-05 18:43:36 +0300

    Added functional to models

--- app/Models/Answer.php
+++ app/Models/Answer.php
@@ -5,8 +5,19 @@ namespace App\Models;
 use App\Models\Interfaces\ModelInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Answer extends Model implements ModelInterface
 {
     use HasFactory;
+
+    protected $fillable = [
+        'question_id',
+        'body',
+    ];
+
+    public function question(): BelongsTo
+    {
+        return $this->belongsTo(Question::class, 'question_id', 'id');
+    }
 }
--- app/Models/Question.php
+++ app/Models/Question.php
@@ -5,8 +5,21 @@ namespace App\Models;
 use App\Models\Interfaces\ModelInterface;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Question extends Model implements ModelInterface
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'body',
+        'm-t',
+        'm-b',
+    ];
+
+    public function answers(): HasMany
+    {
+        return $this->hasMany(Answer::class, 'question_id', 'id');
+    }
 }
